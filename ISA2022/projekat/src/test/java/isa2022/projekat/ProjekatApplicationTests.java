cfa5676fe1e3205ad785e9d452ed11be63cdad14
Aleksandar <mozartkaraokeversion@gmail.com>
2022-10-28 03:14:33 +0200

    dodati plagini za jenkins u pom.xml; dodata dva osnovna testa

--- ISA2022/projekat/src/test/java/isa2022/projekat/ProjekatApplicationTests.java
+++ ISA2022/projekat/src/test/java/isa2022/projekat/ProjekatApplicationTests.java
@@ -1,13 +1,37 @@
 package isa2022.projekat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @SpringBootTest
 class ProjekatApplicationTests {
+	
+	@Autowired
+	private WebApplicationContext webApplicationContext;
 
+	private MockMvc mockMvc;
+	
+	@BeforeEach
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+	
 	@Test
-	void contextLoads() {
+	public void test() {
+		assertEquals(1, 1);
+	}
+	
+	@Test
+	public void primerTest() {
+	    assertEquals(1, 5);
 	}
 
 }
